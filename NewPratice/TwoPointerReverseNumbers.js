4be6cadd1df24714e9d520826720156537b512db
Dinesh <dinesh.tathekalva@outlook.com>
2022-06-22 22:21:07 -0500

    inital commit - TwoPointerReverseNumbers

---
+++ NewPratice/TwoPointerReverseNumbers.js
@@ -0,0 +1,16 @@
+const TwoPointerReverseNumbers = () => {
+    console.clear()
+    const nums = [9,8,7,6,5,4,3,2,1]
+    let start = 0
+    let end = nums.length - 1
+    while (start < end){
+        let temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+        start++
+        end--
+    }
+    return console.log(nums);
+}
+
+TwoPointerReverseNumbers()
